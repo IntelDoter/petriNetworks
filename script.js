a1aa439c5afb19cac05d1ab4b606f27be0fbefb8
inteldoter <inteldoter@gmail.com>
2017-05-19 23:16:27 +0500

    with graphics

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ let transitionOutputArr = [
 ];*/
 
 let positionsWeightArr = [1, 1, 0, 2];
+let isWeightChanged = [false, false ,false, false];
 let transitionWeightArr = [0, 0, 0];
 
 let transitionInputArr = [
@@ -38,25 +39,39 @@ function setNextState() {
     }
 }
 
+function checkChanges() {
+    for (let i = 0; i < isWeightChanged.length; i++) {
+        if (isWeightChanged[i]) {
+            positionsWeightArr[i] -= 1;
+            isWeightChanged[i] = false;
+        }
+    }
+}
+
 function nextState() {
+    clearWeights();
+
     if (state === "Resting") {
         setNextState();
         for (let i = 0; i < transitionInputArr.length; i++) {
             for (let j = 0; j < transitionInputArr[i].length; j++) {
                 let index = transitionInputArr[i][j];
-                let isWeightChanged = false;
 
                 if (positionsWeightArr[index] > 0) {
-                    positionsWeightArr[index] -= 1;
-                    isWeightChanged = true;
-                }
-
-                if (isWeightChanged) {
+                    isWeightChanged[index] = true;
                     transitionWeightArr[i] = transitionOutputArr[i].length;
                 }
             }
         }
 
+        checkChanges();
+
+        clearChips();
+        for (let i = 0; i < transitionWeightArr.length; i++) {
+            if (transitionWeightArr[i] > 0) {
+                addChip("active", i)
+            }
+        }
     } else {
         setNextState();
         for (let i = 0; i < transitionOutputArr.length; i++) {
@@ -65,9 +80,20 @@ function nextState() {
                     let index = transitionOutputArr[i][j];
                     positionsWeightArr[index] += 1;
                 }
-                transitionWeightArr[i] = 0;
+                transitionWeightArr[i] -= 1;
             }
         }
+
+        clearChips();
+        for (let i = 0; i < positionsWeightArr.length; i ++) {
+            if (positionsWeightArr[i] > 0) {
+                addChip("rest", i);
+            }
+        }
+    }
+
+    for (let i = 0; i < positionsWeightArr.length; i++) {
+        drawWeights(i);
     }
 
     console.log(state);
@@ -79,6 +105,117 @@ let btn = document.getElementById("btn_next_state");
 btn.onclick = function() {
     nextState();
 };
-
 console.log(state);
 console.log(positionsWeightArr);
+
+let canvas = new fabric.StaticCanvas('canvas');
+
+let imgElement = document.getElementById('my-image');
+let imgInstance = new fabric.Image(imgElement, {
+    left: 0,
+    top: 0
+});
+canvas.add(imgInstance);
+
+let arrOfObjects = [];
+let arrOfText = [];
+function clearChips() {
+    for (let i = 0; i < arrOfObjects.length; i++) {
+        arrOfObjects[i].remove();
+    }
+    arrOfObjects.length = 0;
+}
+
+function drawWeights(number) {
+    let coordsWeight = {
+        0: {
+            left: 43,
+            top: 230
+        },
+        1: {
+            left: 375,
+            top: 130
+        },
+        2: {
+            left: 630,
+            top: 0
+        },
+        3: {
+            left: 290,
+            top: 350
+        }
+    };
+
+    let text = new fabric.Text(positionsWeightArr[number].toString(), { left: coordsWeight[number].left, top: coordsWeight[number].top });
+    arrOfText.push(text);
+    canvas.add(text);
+}
+
+function clearWeights() {
+    for (let i = 0; i < arrOfText.length; i++) {
+        arrOfText[i].remove();
+    }
+    arrOfText.length = 0;
+}
+
+function addChip(type, num) {
+    let coordsPositions = {
+        0: {
+            left: 43,
+            top: 172
+        },
+        1: {
+            left: 370,
+            top: 70
+        },
+        2: {
+        left: 630,
+            top: 70
+        },
+        3: {
+            left: 300,
+            top: 290
+        }
+    };
+
+    let coordsTransitions = {
+        0 : {
+            left: 175,
+            top: 172
+        },
+        1: {
+            left: 505,
+            top: 50
+        },
+        2: {
+            left: 475   ,
+            top: 300
+        }
+    };
+
+    function drawChip(position) {
+        let circle = new fabric.Circle({
+            radius: 10, left: position.left, top: position.top
+        });
+        arrOfObjects.push(circle);
+        canvas.add(circle);
+    }
+
+    if (type === "rest") {
+        drawChip(coordsPositions[num])
+    } else {
+        drawChip(coordsTransitions[num])
+    }
+}
+
+for (let i = 0; i < positionsWeightArr.length; i ++) {
+    if (positionsWeightArr[i] > 0) {
+        addChip("rest", i);
+    }
+}
+
+for (let i = 0; i < positionsWeightArr.length; i++) {
+    drawWeights(i);
+}
+
+
